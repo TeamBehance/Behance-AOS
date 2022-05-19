d85e259eb4e43042bcd56c1cb0d6c9711bf3dd2c
stopkite <lilyandcat@swu.ac.kr>
2022-05-19 13:04:53 +0900

    #13 [chore] create 화면 drawable 요소 추가

--- app/src/main/java/com/sopt/behance_aos/ui/read/BehanceReadActivity.kt
+++ app/src/main/java/com/sopt/behance_aos/ui/read/BehanceReadActivity.kt
@@ -2,6 +2,7 @@ package com.sopt.behance_aos.ui.read
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sopt.behance_aos.R
 
 class BehanceReadActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

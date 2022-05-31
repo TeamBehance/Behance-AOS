2a06800b91bb038b08a69d268e85bdfad27ef4f6
changhwan <chang5hustle@gmail.com>
2022-05-31 23:07:27 +0900

    #23 [feat] 레트로핏 기초세팅

---
+++ app/src/main/java/com/sopt/behance_aos/data/RetrofitBuilder.kt
@@ -0,0 +1,30 @@
+package com.sopt.behance_aos.data
+
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitBuilder {
+    private const val BASE_URL = "http://13.124.227.66:5000/"
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(10, TimeUnit.SECONDS)
+        .writeTimeout(10, TimeUnit.SECONDS)
+        .readTimeout(10, TimeUnit.SECONDS)
+        .build()
+
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    // 이 밑에다가 이런식으로 서비스 객체 생성하기
+    // val sampleService: SampleService = retrofit.create(SampleService::class.java)
+
+
+}
+

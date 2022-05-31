753168a94906c6c05e31f56dac839a38f7da5b08
stopkite <lilyandcat@swu.ac.kr>
2022-06-01 03:52:58 +0900

    #26 [feat] 갤러리 접근 권한 코드 생성

--- app/src/main/java/com/sopt/behance_aos/ui/create/BehanceCreateActivity.kt
+++ app/src/main/java/com/sopt/behance_aos/ui/create/BehanceCreateActivity.kt
@@ -1,51 +1,77 @@
 package com.sopt.behance_aos.ui.create
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.sopt.behance_aos.R
-import com.sopt.behance_aos.data.GalleryData
-import com.sopt.behance_aos.ui.create.adpater.GalleryAdapter
 
 
 class BehanceCreateActivity : AppCompatActivity() {
 
+
     private lateinit var binding: com.sopt.behance_aos.databinding.ActivityBehanceCreateBinding
-    private lateinit var galleryAdapter: GalleryAdapter
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean ->
+        if (isGranted) { // 권한 획득 성공 시
+            // 이미지 피커 열기
+
+        } else { // 권한 획득 거부 시
+            Toast.makeText(this, "갤러리에 접근하기 위해서는 권한이 필요합니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_behance_create)
-        initGalleryAdapter()
+        requestPermission()
     }
 
-    private fun initGalleryAdapter() {
-        galleryAdapter = GalleryAdapter()
-        val galleryList = mutableListOf<GalleryData>()
 
-        galleryList.addAll(
-            listOf(
-                GalleryData(R.drawable.img_create_gallery1),
-                GalleryData(R.drawable.img_create_gallery2),
-                GalleryData(R.drawable.img_create_gallery3),
-                GalleryData(R.drawable.img_create_gallery4),
-                GalleryData(R.drawable.img_create_gallery5),
-                GalleryData(R.drawable.img_create_gallery6),
-                GalleryData(R.drawable.img_create_gallery7),
-                GalleryData(R.drawable.img_create_gallery8),
-                GalleryData(R.drawable.img_create_gallery9),
-                GalleryData(R.drawable.img_create_gallery10),
-                GalleryData(R.drawable.img_create_gallery11),
-                GalleryData(R.drawable.img_create_gallery12),
-            )
-        )
+    private fun requestPermission() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED -> {  // 이미 권한부여를 받았기에 권한이 필요한 작업 수행
+                // 이미지 피커 열기
+            }
 
-        galleryAdapter.galleryList = galleryList
+            shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE) -> { // 권한 요청 권유
+                showInContextUI()
+            }
 
-        binding.rvCreateGallery.adapter = galleryAdapter
+            else -> {
+                requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)   // 바로 권한요청하는 경우
+            }
+        }
 
-        galleryAdapter.notifyDataSetChanged()
     }
 
+    private fun showInContextUI(){
+        AlertDialog.Builder(this)
+            .setTitle("권한 동의 필요")
+            .setMessage("갤러리에 접근하기 위해서는 권한이 필요합니다.")
+            .setPositiveButton("동의") { _, _ ->
+                requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+            .setNegativeButton("거부") { _, _ ->
+                Toast.makeText(this, "갤러리에 접근하기 위해서는 권한이 필요합니다.", Toast.LENGTH_SHORT).show()
+            }
+            .create()
+            .show()
+    }
 }
